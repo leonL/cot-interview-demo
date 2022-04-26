8c65f5d93f989b5c12baec7c0c3d45be3e9b37a7
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2022-04-26 02:00:19 -0400

    sort cities by populaiton ascending

--- routes/api.js
+++ routes/api.js
@@ -9,17 +9,32 @@ router.get('/provinces', function(req, res, next) {
 });
 
 router.get('/cities/:province', function(req, res, next) {
+  const populationKey = 'Population(2016)';
   const provinceParam = req.params.province.toLocaleLowerCase();
   const cities = db.cities;
+
   const citiesInProvince = cities.filter(city => {
     const cityProvice = city.Province.toLocaleLowerCase();
     return cityProvice === provinceParam;
   });
-  res.json(citiesInProvince);
+
+  const citiesInProviceByPopulation = citiesInProvince.sort((aCity, bCity) => {
+    const aCityPopulation = parseIntegerFromStringWithCommas(aCity[populationKey]);  
+    const bCityPopulation = parseIntegerFromStringWithCommas(bCity[populationKey]);
+    return aCityPopulation - bCityPopulation; // a - b is ascending order
+  }); 
+
+  res.json(citiesInProviceByPopulation);
 });
 
 router.get('/cities', function(req, res, next) {
   res.json(db.cities)
 });
+
+function parseIntegerFromStringWithCommas(string) {
+  const stringWithoutCommas = string.replace(/,/g, '');
+  const integer = Number(stringWithoutCommas);
+  return integer;
+}
 
 module.exports = router;
